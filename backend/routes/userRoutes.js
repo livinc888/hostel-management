995359d1349ece769c0368d74dcd20432cc68b02
livinc888 <livinclivinc888@gmail.com>
2026-04-07 08:54:09 +0530

    fix auth middleware execution issue

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStudents } = require('../controllers/userController');
-const { protect } = require('../middleware);
-
-// ✅ route: /api/users/students
-router.get('/students', protect, getStudents);
-
-module.exports = router;
